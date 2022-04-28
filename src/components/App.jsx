957eff6ff04b4edfb2576a8e4d0b39d307d7edfc
AntoBo <anton.bondarenkooooo@gmail.com>
2022-04-28 17:03:31 +0300

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,27 @@
+import user from '../assets/user';
+import data from '../assets/data';
+import friends from '../assets/friends';
+import transactions from '../assets/transactions';
+
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
+    </>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <li key={id} className="item">
+          <span className="status">{isOnline}</span>
+          <img className="avatar" src={avatar} alt="User avatar" width="48" />
+          <p className="name">{name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default FriendList;
